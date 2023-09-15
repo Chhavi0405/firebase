5ff59b7278e4e3884bb5bac9e532003b7e055792
Chhavi Choudhary <chhavi.2023@tecorb.co>
2023-09-15 18:34:16 +0530

    chat -app

--- chat-app/app/Components/Auth.tsx
+++ chat-app/app/Components/Auth.tsx
@@ -1,27 +1,57 @@
-'use client'
-import {auth,provider} from  '../../firebase.config'
-import {signInWithPopup} from 'firebase/auth'
-import Cookies from 'universal-cookie'
+"use client";
+import { auth, provider } from "../../firebase.config";
+import { signInWithPopup } from "firebase/auth";
+import Cookies from "universal-cookie";
+// import { useRouter } from 'next/router'
+const cookies = new Cookies();
 
-const cookies =new Cookies();
-
-export const Auth =()=> {
-  const signInWithGoogle = async ()=>{
-    try{
-    const result =    await signInWithPopup(auth,provider)
-    cookies.set("auth-token",result.user.refreshToken)
+export const Auth = () => {
+  // const router = useRouter();
+  const signInWithGoogle = async () => {
+    try {
+      const result = await signInWithPopup(auth, provider);
+      cookies.set("auth-token", result.user.refreshToken);
+      location.reload()
+      
+    } catch (err: any) {
+      console.error(err);
     }
-    catch(err:any){
-        console.error(err)
-    }
-  }
+  };
   return (
     <>
-    <p style={{textAlign:'center', color:'red'}}>SignIn with Google to continue</p>
-    <br/>
-    <div style={{textAlign:'center',alignItems:'center'}} >
-    <button type='button' onClick={signInWithGoogle}> Sign In With Google </button>
-    </div>
+      <div>
+        <p
+          style={{
+            textAlign: "center",
+            margin: "25px",
+            color: "red",
+            lineHeight: "200%",
+            background: "lightblue",
+            alignSelf: "center",
+            border: "double",
+            blockSize: "inline",
+          }}
+        >
+        
+          Welcome to Chat App
+        </p>
+      </div>
+      <div style={{ textAlign: "center", alignItems: "center" }}>
+       SignIn with Google to continue...
+      </div>
+      <div
+        style={{
+          textAlign: "center",
+          alignItems: "center",
+          border: "1px solid",
+          margin: "50px",
+        }}
+      >
+        <button type="submit" onClick={signInWithGoogle}>
+     
+          Sign In With Google
+        </button>
+      </div>
     </>
-  )
-}
+  );
+};
--- chat-app/app/page.tsx
+++ chat-app/app/page.tsx
@@ -3,13 +3,14 @@ import { Auth } from "./Components/Auth";
 import Cookies from 'universal-cookie'
 import {useState ,useRef, MutableRefObject} from 'react'
 import { Chat } from "./Components/Chat";
+// import { Route,Routes } from "react-dom";
 const cookies =new Cookies();
 export default function Home() {
  const [isAuth,setIsAuth] = useState(cookies.get('auth-token'))
  const [room,setRoom] = useState("")
- const [isInChat,setIsInChat] = useState(null)
+//  const [isInChat,setIsInChat] = useState(null)
 
- const roomInputRef : any= useRef()
+ const roomInputRef : any= useRef('null')
  if(!isAuth){
   return (
     <>
@@ -17,21 +18,27 @@ export default function Home() {
     </>
   )
  }
+
  return(
   <>
   {room ? (
     <Chat room = {room}/>
   ):(
     <div>
-      <label style={{textAlign:'left' , color:'blue'}}> Enter room name&nbsp;</label>
+      <div>
+      <label style={{textAlign:'left' , color:'blue'}}> Enter room name &nbsp;</label>
       {/* <input  onChange={(e:any)=>setRoom(e.target.value)}/>  */}
       <input ref={roomInputRef}/>
       &nbsp;
-      <div style={{border:'2px' }}>
-      <button  onClick={()=>setRoom(roomInputRef.current.value)} >Enter chat</button>
       </div>
+      <span style={{border:'2px' }}>
+      <button  onClick={()=>setRoom(roomInputRef.current.value)} >Enter chat</button>
+      </span>
     </div>
   )}
+
+
+
   </>
  )
  
